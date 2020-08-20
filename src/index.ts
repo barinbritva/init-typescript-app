ee3be41e15e2b79c72fc199cc112a0c90fa02941
Barin Britva <barinbritva@outlook.com>
2020-08-20 21:54:18 +0300

    Print message after creating app.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import AppGenerator from './app-generator/AppGenerator'
 import AppConfigurator from './app-configurator/AppConfigurator'
+import AppConfig from './app-configurator/AppConfig'
 
-async function createApp (): Promise<void> {
+async function createApp (): Promise<AppConfig> {
   const configurator = new AppConfigurator()
   const appConfig = await configurator.askUser()
 
-  console.debug('appConfig', appConfig)
-
   const generator = new AppGenerator(appConfig)
   await generator.createApp()
+
+  return appConfig
 }
 
 createApp()
+  .then((config: AppConfig) => {
+    const message = '\nIt\'s time to craft!' +
+      `\n\ncd ${config.name}\nnpm install\nnpm run build:dev` +
+      '\n\nFor more information visit https://github.com/barinbritva/create-typescript-app'
+    console.info(message)
+  })
   .catch((error) => {
     console.error('Failed to create app.', error)
   })
